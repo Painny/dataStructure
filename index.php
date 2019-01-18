abf011fc6c2dd98cdb66528bfe0455cf4d5df1c3
pengyu <917103429@qq.com>
2019-01-18 17:23:20 +0800

    测试循环队列

--- index.php
+++ index.php
@@ -8,45 +8,18 @@
 
 require_once "vendor/autoload.php";
 
-//------------二叉搜索树-------------------
-$arr=[10,13,9,12,7,5,8];
-$tree=new Tree\BSTree($arr);
+$queque=new Queque\CircularQueque(3);
 
-echo "元数据:".json_encode($arr)."<br />";
-echo "节点数量:".$tree->getLength()."<br />";
-
-$tmp=new Tree\BSTree();
-$arr=["#","B","#","D","#","A","#","C","#"];
-var_dump($tmp->makeByMidOrder($arr));
-exit;
-
-echo "<hr />";
-
-echo "前序遍历(递归):".json_encode($tree->preThroughByRecursion($tree->getRootNode()))."<br />";
-echo "中序遍历(递归):".json_encode($tree->midThroughByRecursion($tree->getRootNode()))."<br />";
-echo "后序遍历(递归):".json_encode($tree->backThroughByRecursion($tree->getRootNode()))."<br />";
-
-echo "<hr />";
-
-echo "前序遍历(栈):".json_encode($tree->preThroughByStack())."<br />";
-echo "中序遍历(栈):".json_encode($tree->midThroughByStack())."<br />";
-echo "后序遍历(栈):".json_encode($tree->backThrouthByStack())."<br />";
-
-//--------------链表-----------------
-$linkList=new \LinkList\LinkList();
-for ($i=1;$i<=3;$i++){
-    $linkList->insert($i);
-}
-
-var_dump($linkList->delByIndex(1));
-var_dump($linkList->getFirst());
+$queque->push(1);
+$queque->push(2);
+$queque->push(3);
+//
+$queque->pop();
+$queque->pop();
+$queque->pop();
+//
+//$queque->push(4);
 
 
-//--------------栈--------------------
-$expression=new \Stack\Expression();
-var_dump($expression->checkBrackets("(3+2)*(9-4)%2",true));
-var_dump($expression->calculate("(3+2)*(9-4)%2"));
-
-var_dump($expression->checkBrackets("3+((6+3)*7-2)*2",true));
-var_dump($expression->calculate("3+((6+3)*7-2)*2"));
-
+echo $queque->firstIndex;
+echo $queque->endIndex;
